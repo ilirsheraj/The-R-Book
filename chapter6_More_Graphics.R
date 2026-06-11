76a41e1ccf3ad348f07db53b88f73f6c25af08af
Ilir Sheraj <sherajilir@gmail.com>
2026-06-11 23:54:59 +0200

    Updated Chapter 6

--- chapter6_More_Graphics.R
+++ chapter6_More_Graphics.R
@@ -104,3 +104,127 @@ barplot(tapply(response, fact, mean), density = 3:10,
 
 # Fill it in different shades of grey
 barplot(tapply(response, fact, mean), col = grey(seq(0.8, 0.2, length = 8)))
+#################################################################################
+# Changing the looks of Graphics
+x1 <- seq(1:20)
+y1 <- rnorm(length(x1), 10, 2)
+x2 <- seq(1:50)
+y2 <- x2 + rnorm(length(x2), 0, 0.1)
+x3 <- seq(1:10)
+y3 <- rpois(length(x3), 1.8)
+
+par(mfrow=c(1,3))
+plot(x1, y1)
+plot(x2, y2)
+plot(x3, y3)
+par(mfrow = c (1, 1))
+
+# Tick marks
+xvals <- seq(0, 150, 10)
+yvals <- 16 + xvals * 0.4 + rnorm (length (xvals), 0, 6)
+par(mfrow = c (1, 2))
+plot(xvals, yvals, pch = 16, col = hue_pal()(1), 
+     xlab = "x axis label", ylab = "y axis label")
+plot(xvals, yvals, pch = 16, col = hue_pal()(1),
+     xlab = "x axis label", ylab = "y axis label",
+     las = 1, cex.lab = 1.8, cex.axis = 1.5)
+par(mfrow = c(1, 1))
+
+
+# Text font
+## Create empty plot
+plot(1:10, 1:10, type = "n", xlab = "x", ylab = "y")
+# default font
+par(family = "sans")
+text(5, 8, "This is the default font")
+# Change to Serif
+par(family = "serif")
+text(5, 6, "This is the serif font")
+par(family = "mono")
+text(5, 4, "This is the mono font")
+par(family = "HersheySymbol")
+text(5, 2, "This is the symbol font")
+
+# Back to default
+par(family = "sans")
+################################################################################
+# Adding Items to Plots
+map_places <- read.csv("Datasets/map.places.csv", header = T)
+head(map_places)
+attach(map_places)
+
+# This dataset contains coordinates of places
+map_data <- read.csv("Datasets/bowens.csv", header = T)
+head(map_data)
+attach(map_data)
+
+# Shift the coordinates under 60 north by 100 for whatever reason :)
+nn <- ifelse(north < 60, north + 100, north)
+
+# Create an empty plot
+plot(c(20, 100), c(60, 110), type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n")
+# Now populate it with text: this can be handled like a pro by geom_text()
+for (i in 1:length (wanted)){
+  ii <- which(place == as.character(wanted[i]))
+  text(east[ii], nn[ii], as.character(place[ii]), cex = 0.6)
+}
+
+detach(map_places)
+detach(map_data)
+
+# Adding smooth parametric curves
+plotfit <- read.table("Datasets/plotfit.txt", header = T)
+head(plotfit)
+attach(plotfit)
+
+# Remove not to interfere
+rm(list = c("x", "y"))
+
+plot(x, y, pch=16, cex=1.3, col=hue_pal()(3)[1])
+
+# Fit the Rick Curve
+xv <- 0:100
+yA <- 482 * xv * exp (-0.045 * xv)
+yB <- 518 * xv * exp (-0.055 * xv)
+lines(xv, yA, lty = 2, col = hue_pal()(3)[2])
+# Fix the error in the book
+lines(xv, yB, lty = 1, col = hue_pal()(3)[3])
+detach(plotfit)
+
+# Fit non-parametric curves (My favorite)
+jaws <- read.table("Datasets/jaws.txt", header = T)
+head(jaws)
+attach(jaws)
+
+par(mfrow = c (2, 2))
+plot(age, bone, pch = 16, col = hue_pal()(2)[1], main = "Lowess")
+# fit lowess
+lines(lowess(age, bone), col = hue_pal()(2)[2])
+
+# Loess
+plot(age, bone, pch = 16, col = hue_pal()(2)[1], main = "Loess")
+model <- loess(bone ~ age)
+xv <- 0:50
+yv <- predict(model, data.frame(age = xv))
+lines(xv, yv, col = hue_pal()(2)[2])
+
+# GAM
+library(mgcv)
+plot(age, bone, pch = 16, col = hue_pal()(2)[1], main = "GAM")
+model <- gam(bone ~ s(age))
+xv <- 0:50
+yv <- predict(model, list (age = xv))
+lines(xv, yv, col = hue_pal()(2)[2])
+
+# Polynomial
+plot(age, bone, pch = 16, col = hue_pal()(2)[1], main = "Polynomial")
+model <- lm(bone ~ age + I (age^2) + I (age^3))
+xv <- 0:50
+yv <- predict(model, list(age = xv))
+lines(xv, yv, col = hue_pal()(2)[2])
+par(mfrow=c(1,1))
+detach(jaws)
+
+# Connecting Observations
+
+
