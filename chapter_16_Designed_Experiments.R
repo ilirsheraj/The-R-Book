3742ddccdc85ec25c57e83c29c9982a9995edde0
Ilir Sheraj <sherajilir@gmail.com>
2026-09-15 21:41:33 +0200

    ANOVA

--- chapter_16_Designed_Experiments.R
+++ chapter_16_Designed_Experiments.R
@@ -23,3 +23,49 @@ dev.off()
 tapply(growth$gain, list(growth$diet, growth$supplement), mean)
 
 # Then Stick it in the model
+## Use Linear Model with interaction
+growth_mod1 <- lm(gain ~ diet * supplement, data = growth)
+summary(aov(growth_mod1))
+
+## Or use the Anova Function
+model_aov <- aov(gain ~ diet * supplement, data = growth)
+summary(model_aov)
+
+# Intercept is the overall mean with both factor levels: diet=barley, supplement=control!
+summary(growth_mod1)
+
+# Remove the interaction term
+model_aov2 <- aov(gain ~ diet + supplement, data = growth)
+summary(model_aov2)
+
+growth_mod2 <- lm(gain ~ diet + supplement, data = growth)
+summary(growth_mod2)
+
+# supergain + control -> worst
+# agrimore + supersup -> best
+supp_new <- growth$supplement
+levels(supp_new)
+levels(supp_new)[c(2,4)] <- "best"
+levels(supp_new)[c(1,3)] <- "worst"
+levels(supp_new)
+levels(supp_new)
+
+growth <- cbind(growth, supp_new)
+head(growth)
+tapply(growth$gain, list(growth$diet, growth$supp_new), mean)
+
+growth_mod3 <- lm(gain ~ diet + supp_new, data = growth)
+summary(growth_mod3)
+
+# Compare to model 2
+anova(growth_mod3, growth_mod2)
+
+# Data Expansion for factor levels: 5 x 5 x 2
+expanded_factors <- expand.grid(height = seq(60, 80, 5),
+                                weight = seq(100, 300, 50),
+                                sex = c ("Male", "Female"))
+head(expanded_factors)
+tail(expanded_factors)
+
+################################################################################
+# Part 2: Pseudo-Replication
